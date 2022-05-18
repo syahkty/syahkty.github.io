ea91efbd380d91875b3b56485750e7a4e504837d
syahkty <andisyahktu46@gmail.com>
2022-05-18 17:19:00 +0700

    angkot

---
+++ skerip.js
@@ -0,0 +1,13 @@
+alert("kamu mau ngak");
+var lag = false;
+var nama = prompt("Nama Kamu");
+while (lag === false) {
+  lag = confirm(nama + " mau ngak jdi pacar aku");
+}
+alert("yey kita jadian" + " " + nama);
+var angkot = 1;
+
+while (angkot <= 5) {
+  console.log("Angkot No" + angkot + "beroprasi dengan baik");
+}
+angkot++;
